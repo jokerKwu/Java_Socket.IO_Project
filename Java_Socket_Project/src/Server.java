dd13594029ec9d3cd403e8acb843342d0ce5669b
joker <south485@daum.net>
2019-09-14 19:59:57 +0900

    author test2

--- Java_Socket_Project/src/Server.java
+++ Java_Socket_Project/src/Server.java
@@ -3,9 +3,7 @@ public class Server {
 	public static void main(String[] args) {
 		System.out.println("테스트");
 		System.out.println("테스트2");
-		System.out.println("테스트3");
-		System.out.println("테스트4");
-		System.out.println("테스트5");
+		
 		System.out.println("테스트6");
 	}
 }
