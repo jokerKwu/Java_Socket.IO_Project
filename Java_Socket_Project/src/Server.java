2726144b84797e0877a4850f4670bf3426ea13d1
jokerKwu <pkjhj485@naver.com>
2019-09-14 20:13:44 +0900

    author test

--- Java_Socket_Project/src/Server.java
+++ Java_Socket_Project/src/Server.java
@@ -3,8 +3,6 @@ public class Server {
 	public static void main(String[] args) {
 		System.out.println("테스트");
 		System.out.println("테스트2");
-		System.out.println("테스트3");
-		System.out.println("테스트4");
 		System.out.println("테스트5");
 	}
 }
