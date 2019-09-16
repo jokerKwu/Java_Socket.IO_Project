0818b1b491999a172a6076867da4a698f25de020
jokerKwu <pkjhj485@naver.com>
2019-09-16 09:27:44 +0900

    final commit test

--- Java_Socket_Project/src/Server.java
+++ Java_Socket_Project/src/Server.java
@@ -1,13 +1,6 @@
 
 public class Server {
 	public static void main(String[] args) {
-		System.out.println("테스트");
-		System.out.println("테스트2");
-<<<<<<< HEAD
-		
-		System.out.println("테스트6");
-=======
-		System.out.println("테스트5");
->>>>>>> joker
+
 	}
 }
