87fd4a5e818f379c2754bc0ff1ad293e87e7ffc9
jokerKwu <pkjhj485@naver.com>
2019-09-16 10:33:08 +0900

    server client ui complete

--- Java_Socket_Project/src/Client.java
+++ Java_Socket_Project/src/Client.java
@@ -8,7 +8,9 @@ import java.net.Socket;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
@@ -17,6 +19,19 @@ import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Client extends Application {
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception{
+		Parent root= (Parent)FXMLLoader.load(getClass().getResource("clientUI.fxml"));
+		Scene scene=new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("clientStyle.css").toString());
+		
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Client");
+		primaryStage.show();
+	}
+	
+	/*
 	Socket socket;
 	
 	void startClient() {
@@ -148,6 +163,8 @@ public class Client extends Application {
 	void displayText(String text) {
 		txtDisplay.appendText(text + "\n");
 	}	
+	
+	*/
 	
 	public static void main(String[] args) {
 		launch(args);
--- Java_Socket_Project/src/Server.java
+++ Java_Socket_Project/src/Server.java
@@ -1,26 +1,23 @@
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Vector;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.geometry.Insets;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Server extends Application {
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception{
+		Parent root= (Parent)FXMLLoader.load(getClass().getResource("serverUI.fxml"));
+		Scene scene=new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("serverStyle.css").toString());
+		
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Server");
+		primaryStage.show();
+	}
+	/*
 	ExecutorService executorService;
 	ServerSocket serverSocket;
 	List<Client> connections = new Vector<Client>();
@@ -190,7 +187,7 @@ public class Server extends Application {
 	void displayText(String text) {
 		txtDisplay.appendText(text + "\n");
 	}	
-	
+	*/
 	public static void main(String[] args) {
 		launch(args);
 	}
