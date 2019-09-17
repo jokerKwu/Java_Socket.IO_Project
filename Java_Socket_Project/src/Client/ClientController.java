e8a8eb29143112cb8a3b8605111db172c445871c
jokerKwu <pkjhj485@naver.com>
2019-09-18 00:59:05 +0900

    server & client connect list fix

--- Java_Socket_Project/src/Client/ClientController.java
+++ Java_Socket_Project/src/Client/ClientController.java
@@ -14,49 +14,54 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Alert;
-import  javafx.scene.control.Button;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import  javafx.scene.control.TextArea;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+public class ClientController implements Initializable {
 
-public class ClientController implements Initializable{
-	
-	@FXML private Button connBtn;
-	@FXML private Button receiveBtn;
-	@FXML private ComboBox uidComboBox;
-	@FXML private TextArea clientLog;
-	@FXML private TextField clientInput;
-	@FXML private Button sendBtn;
-	@FXML private TextField userIdInput;
-	
+	@FXML
+	private Button connBtn;
+	@FXML
+	private Button receiveBtn;
+	@FXML
+	private ComboBox uidComboBox;
+	@FXML
+	private TextArea clientLog;
+	@FXML
+	private TextField clientInput;
+	@FXML
+	private Button sendBtn;
+	@FXML
+	private TextField userIdInput;
+
 	Socket socket;
-	
+
 	@Override
-	public void initialize(URL location,ResourceBundle resources) {
-		
-		
-		connBtn.setOnAction(event->handleClientBtnAction(event));
-		
-		sendBtn.setOnAction(event->handleClientMessageSendAction(event));
+	public void initialize(URL location, ResourceBundle resources) {
+
+		connBtn.setOnAction(event -> handleClientBtnAction(event));
+
+		sendBtn.setOnAction(event -> handleClientMessageSendAction(event));
 	}
-	
+
 	public void handleClientMessageSendAction(ActionEvent event) {
-		String text="";
-		if(clientInput.getText()!=null) {
-			text=clientInput.getText();
+		String text = "";
+		if (clientInput.getText() != null) {
+			text = clientInput.getText();
 			send(text);
 		}
 	}
-	
+
 	public void handleClientBtnAction(ActionEvent event) {
-		if(connBtn.getText().equals("connect")) {
+		if (connBtn.getText().equals("connect")) {
 			startClient();
-		} else if(connBtn.getText().equals("disconnect")){
+		} else if (connBtn.getText().equals("disconnect")) {
 			stopClient();
 		}
 	}
-	
+
 	// 클라이언트 프로그램의 작동을 시작하는 메소드
 	void startClient() {
 		
@@ -66,12 +71,11 @@ public class ClientController implements Initializable{
 				try {
 					socket = new Socket();
 					
-					String userId=userIdInput.getText().toString();
-					System.out.println(userId.length());
+					String userId;
 					
-					// 서버와 연결 시도
-					// 서버 연결 성공
-					if(userId.length()!=0&&!userId.equals("아이디 입력")) {
+					userId=stringProcess("id",userIdInput.getText());
+					// 서버 연결 시도 서버 연결 성공
+					if(userIdInput.getText().length()!=0&&!userIdInput.getText().equals("아이디 입력")) {
 						socket.connect(new InetSocketAddress("localhost", 5001));
 						Platform.runLater(() -> {
 							displayText("[연결 완료: " + socket.getRemoteSocketAddress() + "]");
@@ -97,71 +101,88 @@ public class ClientController implements Initializable{
 		};
 		thread.start();
 	}
-	
+
 	// 클라이언트 프로그램의 작동을 종료하는 메소드
 	void stopClient() {
 		try {
-			Platform.runLater(()->{
+			Platform.runLater(() -> {
 				displayText("[연결 끊음]");
 				connBtn.setText("connect");
 				sendBtn.setDisable(true);
 			});
 			userIdInput.setDisable(false);
-			if(socket!=null && !socket.isClosed()) {
+			if (socket != null && !socket.isClosed()) {
 				socket.close();
 			}
-		} catch (IOException e) {}
-	}	
-	
+		} catch (IOException e) {
+		}
+	}
+
 	// 서버로부터 메시지를 전달받는 메소드
 	void receive() {
-		while(true) {
+		while (true) {
 			try {
 				byte[] byteArr = new byte[100];
 				InputStream inputStream = socket.getInputStream();
-				
-				//서버가 비정상적으로 종료했을 경우 IOException 발생
+
+				// 서버가 비정상적으로 종료했을 경우 IOException 발생
 				int readByteCount = inputStream.read(byteArr);
-				
-				//서버가 정상적으로 Socket의 close()를 호출했을 경우
-				if(readByteCount == -1) { throw new IOException(); }
-				
+
+				// 서버가 정상적으로 Socket의 close()를 호출했을 경우
+				if (readByteCount == -1) {
+					throw new IOException();
+				}
+
 				String data = new String(byteArr, 0, readByteCount, "UTF-8");
-				
-				Platform.runLater(()->displayText("[받기 완료] "  + data));
+
+				Platform.runLater(() -> displayText("[받기 완료] " + data));
 			} catch (Exception e) {
-				Platform.runLater(()->displayText("[서버 통신 안됨]"));
+				Platform.runLater(() -> displayText("[서버 통신 안됨]"));
 				stopClient();
 				break;
 			}
 		}
 	}
-	
+
 	// 서버로 메시지를 전송하는 메소드
 	void send(String data) {
 		Thread thread = new Thread() {
 			@Override
 			public void run() {
-				try {		
+				try {
 					byte[] byteArr = data.getBytes("UTF-8");
 					OutputStream outputStream = socket.getOutputStream();
 					outputStream.write(byteArr);
 					outputStream.flush();
-					Platform.runLater(()->displayText("[보내기 완료]"));
-				} catch(Exception e) {
-					Platform.runLater(()->displayText("[서버 통신 안됨]"));
+					Platform.runLater(() -> displayText("[보내기 완료]"));
+				} catch (Exception e) {
+					Platform.runLater(() -> displayText("[서버 통신 안됨]"));
 					stopClient();
-				}				
+				}
 			}
 		};
 		thread.start();
 	}
 
-	//클라이언트 로그창에 메시지 기록하는 메소드
+	// 클라이언트 로그창에 메시지 기록하는 메소드
 	void displayText(String text) {
-	
+
 		clientLog.appendText(text + "\n");
-	}	
-	
-	
+	}
+
+	String stringProcess(String cmd,String msg) {
+		String res = new String();
+		switch (cmd) {
+			case "id":
+				res = "id//";
+				break;
+			case "send":
+				res = "send//";
+				break;
+			case "receive":
+				res = "receive//";
+				break;
+		}
+		return res+msg;
+	}
 }
--- Java_Socket_Project/src/Server/ServerController.java
+++ Java_Socket_Project/src/Server/ServerController.java
@@ -1,4 +1,5 @@
 package Server;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -18,154 +19,169 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import  javafx.scene.control.Button;
-import  javafx.scene.control.TextArea;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 
-public class ServerController implements Initializable{
-	@FXML private TextArea serverLog;
-	@FXML private Button serverBtn;
-	@FXML private TextArea connectionList;
-	
+public class ServerController implements Initializable {
+	@FXML
+	private TextArea serverLog;
+	@FXML
+	private Button serverBtn;
+	@FXML
+	private TextArea connectionList;
+
 	ExecutorService executorService;
 	ServerSocket serverSocket;
 	List<Client> connections = new Vector<Client>();
-	HashMap<String,OutputStream> hm=new HashMap<String,OutputStream>();
-	
-	
+	HashMap<String, OutputStream> hm = new HashMap<String, OutputStream>();
+
 	@Override
-	public void initialize(URL location,ResourceBundle resources) {
-		
-		serverBtn.setOnAction(event->handleServerBtnAction(event));
-		
-		
+	public void initialize(URL location, ResourceBundle resources) {
+
+		serverBtn.setOnAction(event -> handleServerBtnAction(event));
+
 	}
-	
+
 	public void handleServerBtnAction(ActionEvent event) {
-		if(serverBtn.getText().equals("start")) {
+		if (serverBtn.getText().equals("start")) {
 			startServer();
-		}else if(serverBtn.getText().equals("stop")) {
+		} else if (serverBtn.getText().equals("stop")) {
 			stopServer();
 		}
 	}
-	
-	//서버를 구동시켜 클라이언트의 연결을 기다리는 메소드
+
+	// 서버를 구동시켜 클라이언트의 연결을 기다리는 메소드
 	void startServer() {
-		executorService = Executors.newFixedThreadPool(
-			Runtime.getRuntime().availableProcessors()
-	    );
-		
+		executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
 		try {
-			serverSocket = new ServerSocket();		
+			serverSocket = new ServerSocket();
 			serverSocket.bind(new InetSocketAddress("localhost", 5001));
-		} catch(Exception e) {
-			if(!serverSocket.isClosed()) { stopServer(); }
+		} catch (Exception e) {
+			if (!serverSocket.isClosed()) {
+				stopServer();
+			}
 			return;
 		}
-		
+
 		// 클라이언트가 접속할 때까지 계속 기다리는 쓰레드
 		Runnable runnable = new Runnable() {
 			@Override
 			public void run() {
-				Platform.runLater(()->{
+				Platform.runLater(() -> {
 					serverBtn.setText("stop");
 					serverLogText("[서버 시작]");
-				});		
-				while(true) {
+				});
+				while (true) {
 					try {
-						//클라이언트 연결을 기다리고있다.
+						// 클라이언트 연결을 기다리고있다.
 						Socket socket = serverSocket.accept();
-						String message = "[연결 수락: " + socket.getRemoteSocketAddress()  + ": " + Thread.currentThread().getName() + "]";
-						Platform.runLater(()->serverLogText(message));
-					
+						String message = "[연결 수락: " + socket.getRemoteSocketAddress() + ": "
+								+ Thread.currentThread().getName() + "]";
+						Platform.runLater(() -> serverLogText(message));
+
 						Client client = new Client(socket);
 						connections.add(client);
-						Platform.runLater(()->serverLogText("[연결 개수: " + connections.size() + "]"));
+						Platform.runLater(() -> serverLogText("[연결 개수: " + connections.size() + "]"));
 					} catch (Exception e) {
-						if(!serverSocket.isClosed()) { stopServer(); }
+						if (!serverSocket.isClosed()) {
+							stopServer();
+						}
 						break;
 					}
 				}
 			}
- 
+
 		};
 		executorService.submit(runnable);
 	}
-	
-	//서버의 작동을 중지시키는 메소드
+
+	// 서버의 작동을 중지시키는 메소드
 	void stopServer() {
 		try {
-			//현재 작동 중인 모든 소켓 닫기
+			// 현재 작동 중인 모든 소켓 닫기
 			Iterator<Client> iterator = connections.iterator();
-			while(iterator.hasNext()) {
+			while (iterator.hasNext()) {
 				Client client = iterator.next();
 				client.socket.close();
 				iterator.remove();
 			}
 			// 서버 소켓 객체 닫기
-			if(serverSocket!=null && !serverSocket.isClosed()) { 
-				serverSocket.close(); 
+			if (serverSocket != null && !serverSocket.isClosed()) {
+				serverSocket.close();
 			}
 			// 쓰레드 풀 종료하기
-			if(executorService!=null && !executorService.isShutdown()) { 
-				executorService.shutdown(); 
+			if (executorService != null && !executorService.isShutdown()) {
+				executorService.shutdown();
 			}
-			Platform.runLater(()->{
+			Platform.runLater(() -> {
 				serverLogText("[서버 멈춤]");
 				serverBtn.setText("start");
 			});
-		} catch (Exception e) { }
-	}	
-	
-	class Client {
-		Socket socket;	//서버와 통신할 소켓
-		String userID;
-		Client(Socket socket) {
-			this.socket = socket; //서버와 통신할 소켓 저장
-			send("테스트2");
-			receive();			//메시지 받는다.
-			
+		} catch (Exception e) {
 		}
-		
+	}
+
+	class Client {
+		Socket socket; // 서버와 통신할 소켓
+		String userID;
+
+		Client(Socket socket) {
+			this.socket = socket; // 서버와 통신할 소켓 저장
+			receive(); // 메시지 받는다.
+
+		}
+
 		void receive() {
 			Runnable runnable = new Runnable() {
 				@Override
 				public void run() {
 					try {
-						while(true) {
+						while (true) {
 							byte[] byteArr = new byte[100];
 							InputStream inputStream = socket.getInputStream();
-							
-							//클라이언트가 비저상 종료를 했을 경우 IOException 발생
+
+							// 클라이언트가 비저상 종료를 했을 경우 IOException 발생
 							int readByteCount = inputStream.read(byteArr);
-							
-							//클라이언트가 정상적으로 Socket의 close()를 호출했을 경우
-							if(readByteCount == -1) {  throw new IOException(); }
-							
-							String message = "[요청 처리: " + socket.getRemoteSocketAddress() + ": " + Thread.currentThread().getName() + "]";
-							Platform.runLater(()->serverLogText(message));
-							
-							String data = new String(byteArr, 0, readByteCount, "UTF-8");
-							userID=data;
-							
-							//접속 목록을 위해서 저장
-							hm.put(data, socket.getOutputStream());
-							
-							Platform.runLater(()->serverLogText(data+"님이 접속하셨습니다."));
-							Platform.runLater(()->connectedListText(hm));
-							for(Client client : connections) {
-								client.send(data+"님이 접속하셨습니다."); 
+
+							// 클라이언트가 정상적으로 Socket의 close()를 호출했을 경우
+							if (readByteCount == -1) {
+								throw new IOException();
 							}
+
+							String message = "[요청 처리: " + socket.getRemoteSocketAddress() + ": "
+									+ Thread.currentThread().getName() + "]";
+							Platform.runLater(() -> serverLogText(message));
+
+							String data = new String(byteArr, 0, readByteCount, "UTF-8");
+							
+							String[] strArr = data.split("//");
+							
+							if(strArr[0]=="id") {Client.this.userID=strArr[1];
+							System.out.println("서버테스트1 "+strArr[1]);
+							System.out.println("서버테스트2 "+Client.this.userID);
+							}
+							System.out.println("테스트다 임마 "+Client.this.userID);
+							// 클라이언트한테 전달받은 메시지 처리
+							receiveMessageProcess(data);
+
+							// hm.put(data, socket.getOutputStream());
+
 						}
-					} catch(Exception e) {
+					} catch (Exception e) {
 						try {
+							if(Client.this.userID.equals("조현준")) System.out.println("모여 시바");
 							hm.remove(Client.this.userID);
 							connections.remove(Client.this);
-							String message = "[클라이언트 통신 안됨: " + socket.getRemoteSocketAddress() + ": " + Thread.currentThread().getName() + "]";
-							Platform.runLater(()->serverLogText(message));
-							Platform.runLater(()->connectedListText(hm));
-							
+							Platform.runLater(() -> connectedListText(hm));
+							String message = "[2.클라이언트 통신 안됨: " + socket.getRemoteSocketAddress() + ": "
+									+ Thread.currentThread().getName() + "]";
+							Platform.runLater(() -> serverLogText(message));
+
+
 							socket.close();
-						} catch (IOException e2) {}
+						} catch (IOException e2) {
+						}
 					}
 				}
 			};
@@ -181,31 +197,52 @@ public class ServerController implements Initializable{
 						OutputStream outputStream = socket.getOutputStream();
 						outputStream.write(byteArr);
 						outputStream.flush();
-					} catch(Exception e) {
+					} catch (Exception e) {
 						try {
-							String message = "[클라이언트 통신 안됨: " + socket.getRemoteSocketAddress() + ": " + Thread.currentThread().getName() + "]";
-							Platform.runLater(()->serverLogText(message));
+							String message = "[3.클라이언트 통신 안됨: " + socket.getRemoteSocketAddress() + ": "
+									+ Thread.currentThread().getName() + "]";
+							Platform.runLater(() -> serverLogText(message));
 							connections.remove(Client.this);
 							socket.close();
-						} catch (IOException e2) {}
+						} catch (IOException e2) {
+						}
 					}
 				}
 			};
 			executorService.submit(runnable);
 		}
 
+		void receiveMessageProcess(String data) throws IOException {
+			String[] strArr = data.split("//");
+			switch (strArr[0]) {
+			case "id":
+				hm.put(strArr[1], socket.getOutputStream());
+				Platform.runLater(() -> serverLogText(strArr[1] + "님이 접속하셨습니다."));
+				Platform.runLater(() -> connectedListText(hm));
+				for (Client client : connections) {
+					client.send(strArr[1] + "님이 접속하셨습니다.");
+				}
+				
+				break;
+			case "send":
+				break;
+			case "receive":
+				break;
+			}
+		}
+
 	}
-	
+
 	// 서버 로그창 기록 메소드
 	void serverLogText(String text) {
 		serverLog.appendText(text + "\n");
-	}	
-	
+	}
+
 	// 서버 접속자 기록 메소드
-	void connectedListText(HashMap<String,OutputStream> cl) {
+	void connectedListText(HashMap<String, OutputStream> cl) {
 		connectionList.clear();
 		Iterator<String> keys = hm.keySet().iterator();
-		while( keys.hasNext() ) {
+		while (keys.hasNext()) {
 			String userID = keys.next();
 			connectionList.appendText(userID + "\n");
 		}
