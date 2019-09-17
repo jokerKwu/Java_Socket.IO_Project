92c73ed71fd13a370a5bec0c49fedc96cfb4fadd
jokerKwu <pkjhj485@naver.com>
2019-09-17 17:32:49 +0900

    Client UI fix

--- Java_Socket_Project/src/Client/Client.java
+++ Java_Socket_Project/src/Client/Client.java
@@ -1,22 +1,12 @@
 package Client;
 
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
-import java.net.Socket;
-
 import javafx.application.Application;
-import javafx.application.Platform;
+
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
+
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Client extends Application {
